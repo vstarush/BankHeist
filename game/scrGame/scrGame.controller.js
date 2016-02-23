691f379f5fec877b6e0cfc0fba5fbcedd250f7c5
vstarush <vstarush@gmail.com>
2016-02-22 22:48:00 -0500

    test with popups
    test

--- game/scrGame/scrGame.controller.js
+++ game/scrGame/scrGame.controller.js
@@ -1,4 +1,38 @@
 app.controller('scrGameController', function($rootScope, $scope){
     
     $scope.testMessage = 'This is Game Test';
+    
+    $scope.numbers = [];
+    var counter = 0;
+    
+    $scope.AddNumber = function(){
+        $scope.numbers.push(counter);
+        counter+=1;
+        $scope.$apply();
+    };
+    
+    $scope.ActivateInterval = function(){
+        setInterval($scope.AddNumber,4000);
+    }
 });
+
+/*
+SceneName = {
+    callToAction:'',
+    choiceType:'',
+    description:'',
+    nextSceneOne:'',
+    nextSceneTwo:'',
+    showTimeOut:''
+}
+
+CurrentScene = SceneName;
+
+function playgame(){
+    
+}
+
+function call(scene){
+    
+}
+*/
