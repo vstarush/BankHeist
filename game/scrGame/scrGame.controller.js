c896894858fa20b05dcbeefe398bd1eec8bf4d05
vstarush <vstarush@gmail.com>
2016-02-26 20:30:25 -0500

    scene obj
    done

--- game/scrGame/scrGame.controller.js
+++ game/scrGame/scrGame.controller.js
@@ -1,24 +1,85 @@
 app.controller('scrGameController', function($rootScope, $scope){
     
-    $scope.testMessage = 'This is Game Test';
+    $scope.completed_scenes = [];
+
+    function game_function(scene){
+        
+        $scope.completed_scenes.push(scene);
+        //$scope.$apply();
+        //console.log($scope.completed_scenes.length);
+        
+        if(scene.next_scene() === 'end'){
+            return 'end';
+        }
+        
+//        var all_game_containers = document.getElementsByClassName('game_text_container');
+//        var last_element = all_game_containers[all_game_containers.length-1];
+//        last_element.scrollIntoView();
+        
+        setTimeout(function(){
+            game_function(scene.next_scene())
+            $scope.$apply();
+        },scene.scene_timeout);        
+    }
     
-    $scope.numbers = [];
-    var counter = 0;
-    
-    $scope.AddNumber = function(){
-        $scope.numbers.push(counter);
-        counter+=1;
-        $scope.$apply();
-        var all_game_containers = document.getElementsByClassName('game_text_container');
-        var last_element = all_game_containers[all_game_containers.length-1];
-        //$(last_element).scrollIntoView();
-        last_element.scrollIntoView();
+    $scope.start_game = function(){
+        game_function(test_scene);
     };
     
-    $scope.ActivateInterval = function(){
-        setInterval($scope.AddNumber,4000);
-    }
+    
+    
+      
+    var test_scene = {
+        scene_id:1,
+        next_scene:function(){
+            return test_scene_2;
+        },
+        scene_type:'simple',
+        scene_text:'He killed that mutherfucker and dumped the body in a near by river',
+        scene_timeout:4000
+    };
+    
+    var test_scene_2 = {
+        scene_id:2,
+        next_scene:function(){
+            return test_scene_3;
+        },
+        scene_type:'simple',
+        scene_text:'The police had no idea where to start the investigation',
+        scene_timeout:2000
+    };
+    
+    var test_scene_3 = {
+        scene_id:3,
+        next_scene:function(){
+            return 'end';
+        },
+        scene_type:'simple',
+        scene_text:'The search brought no results',
+        scene_timeout:2000
+    };
+    
 });
+
+
+
+//$scope.testMessage = 'This is Game Test';
+
+    //var scene_counter = 0;
+    
+//    $scope.AddNumber = function(current_scene){
+//        $scope.completed_scenes.push(current_scene);
+//        //counter+=1;
+//        $scope.$apply();
+//        var all_game_containers = document.getElementsByClassName('game_text_container');
+//        var last_element = all_game_containers[all_game_containers.length-1];
+//        last_element.scrollIntoView();
+//    };
+
+
+//    $scope.ActivateInterval = function(){
+//        setInterval($scope.AddNumber(),4000);
+//    }
 
 /*
 SceneName = {
